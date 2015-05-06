3890f33ba89a725325c565af32015abbc6319305
Mike McDonald <mcdonald@firebase.com>
2015-05-06 15:05:04 -0700

    Adding in backend code for chat message sanitization

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+var Queue = require('firebase-queue'),
+    Firebase = require('firebase'),
+    Filter = require('bad-words');
+
+var ref = new Firebase('https://sanichat.firebaseio.com/');
+ref.authWithCustomToken('<your-token-here>', function(err, authData){
+    if (err) {
+      console.log("Login failed with error: ", error);
+    } else {
+      console.log("Authenticated successfully with payload: ", authData);
+    }
+});
+var queue = new Queue(ref, function(data, progress, resolve, reject) {
+  var filter = new Filter();
+
+  data.message = filter.clean(data.message);
+
+  ref.child('messages').push(data, function(err){
+    if (err) {
+        reject(err);
+    } else {
+        resolve(data);
+    }
+  });
+});
